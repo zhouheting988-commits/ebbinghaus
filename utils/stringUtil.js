ee2a9928f544bd366a7f6feae2b34a2f336a7a8c
zhouheting988-commits <zhouheting988@gmail.com>
2025-10-22 15:32:52 +0800

    Create stringUtil.js

---
+++ utils/stringUtil.js
@@ -0,0 +1,33 @@
+import { USER } from "../core/manager.js";
+
+/**
+ * 替换字符串中的user标签 (保留)
+ */
+export function replaceUserTag(str) {
+    // ... (代码不变)
+}
+
+/**
+ * 将单元格中的逗号替换为/符号 (保留)
+ */
+export function handleCellValue(cell) {
+    // ... (代码不变)
+}
+
+/**
+ * 截断最后的括号后的内容 (保留，可能有用)
+ */
+export function truncateAfterLastParenthesis(str) {
+    // ... (代码不变)
+}
+
+/**
+ * 解析字符串字典为对象 (核心功能，必须保留)
+ * @param {*} str
+ * @returns object
+ */
+export function parseLooseDict(str) {
+    // ... (代码不变)
+}
+
+// 移除了 parseManualJson 和 safeParse 两个函数
